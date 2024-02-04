b3821ff1c6872b166811609a373d39027efd15bb
ridvangulce <ridvangulce@gmail.com>
2024-02-04 16:20:46 +0300

    Created Models

---
+++ server/models/Room.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    maxPeople: {
+        type: Number,
+        required: true
+    },
+    roomNumbers: [{
+        number: Number,
+        unavailableDates: {
+            type: [Date]
+        }
+    }],
+}, { timestamps: true })
+
+module.exports = mongoose.model('Room', roomSchema);
